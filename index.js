143ee59aaf92591c40096a0a7e1192e85da3220f
NeverBehave <gayhub@never.pet>
2020-05-23 17:58:50 -0400

    fix: img link

--- index.js
+++ index.js
@@ -14,11 +14,12 @@ const testUrl = (str) => {
 const testFileExist = (str) =>
   existsSync(path.resolve(__dirname, "src/friends", str));
 
+const imgPrefix = 'https://cdn.jsdelivr.net/gh/homeofnever/friends@gh-pages/friends/'
 
 // Get document, or throw exception on error
 // Exit on 1 to stop CI
 let friends = {};
-consola.info('Start reading yaml')
+consola.info("Start reading yaml");
 try {
   friends = yaml.safeLoad(
     readFileSync(path.resolve(__dirname, "src/friends.yml"), "utf8")
@@ -30,53 +31,55 @@ try {
 }
 
 // Test img
-consola.info('Start testing img')
+consola.info("Start testing img");
 try {
   Object.keys(friends).forEach((e) => {
-    if (
-      !(
-        friends[e].img === null ||
-        testUrl(friends[e].img) ||
-        testFileExist(friends[e].img)
-      )
-    )
-      throw new Error(friends[e].img + " is invaild");
+    if (!(friends[e].img === null || testUrl(friends[e].img))) {
+        if (testFileExist(friends[e].img)) {
+            friends[e].img = imgPrefix + friends[e].img
+        } else {
+            throw new Error(friends[e].img + " is invaild");
+        }
+    }
   });
   consola.success("nice, everyone has a valid img!");
+  consola.info(friends)
 } catch (e) {
   consola.error(e);
   process.exit(1);
 }
 
 // Test blog's connectivity
-consola.info('Start testing blog connectivity')
+consola.info("Start testing blog connectivity");
 const responses = Object.keys(friends)
   .filter((e) => friends[e].test !== false)
   .map((e) => {
     return got(friends[e].link, {
-        timeout: 15000
-    }).then((res) => {
-      return {
-        link: friends[e].link,
-        response: res.body.substring(0, 50),
-      };
-    }).catch(err => {
-        consola.error(err)
-        throw new Error(`${friends[e].link} failed the request`)
-    });
+      timeout: 15000,
+    })
+      .then((res) => {
+        return {
+          link: friends[e].link,
+          response: res.body.substring(0, 50),
+        };
+      })
+      .catch((err) => {
+        consola.error(err);
+        throw new Error(`${friends[e].link} failed the request`);
+      });
   });
 
 // Write file on all success
 Promise.all(responses)
   .then((values) => {
-    consola.info(values)
+    consola.info(values);
     consola.success("nice, everyone looks healthy");
-    consola.info("Start writing json")
+    consola.info("Start writing json");
     writeFileSync(
       path.resolve(__dirname, "public/friends.json"),
       JSON.stringify(friends)
     );
-    console.success("Finished writing json")
+    consola.success("Finished writing json");
   })
   .catch((e) => {
     consola.error(e);
